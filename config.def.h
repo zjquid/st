92e416fc0d133c8a78858c2f6a992aaad285f07c
zjquid <39973225+zjquid@users.noreply.github.com>
2021-03-04 01:39:53 -0600

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:size=12:antialias=false";
+static char *font = "Inconsolata:size=12:antialias=true";
 static int borderpx = 0;
 
 /*
